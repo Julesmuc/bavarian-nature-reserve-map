c8de5ad837fac0cb2808f47d996f650e95504ee2
julian <geschroeteter@gmail.com>
2020-11-24 12:46:11 +0100

    fly to reserves

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 import './App.css';
 import DeckGL from '@deck.gl/react';
-import { InteractiveMap } from 'react-map-gl';
+import {StaticMap} from 'react-map-gl';
 import { GeoJsonLayer } from '@deck.gl/layers';
+import {FlyToInterpolator} from '@deck.gl/core';
 import { apitoken } from './apitoken'
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
@@ -17,20 +18,72 @@ const INITIAL_VIEW_STATE = {
 };
 
 
-function App() {
+const LayerList = ({ layers, viewport }) => {
 
-const [getLayer,setLayer] = useState(null);
+  const flyToReserve = (layer) => {
+    viewport({
+      longitude: layer.geometry.coordinates[0][0][0][0],
+      latitude: layer.geometry.coordinates[0][0][0][1],
+      zoom: 14,
+      pitch: 0,
+      bearing: 0,
+      transitionDuration: 2000,
+      transitionInterpolator: new FlyToInterpolator()
+    })
+    console.log(`I AM: ${layer.properties.name}`);
+  }
 
-async function getNatureLayer() {
-  const response =  await fetch(DATA_URL);
-  const json = await response.json();
-  setLayer(json);
+  const getNatureReserveList = () => {
+    if (layers) {
+      return layers.features.map((layer, id) => {
+        return (
+          <div onClick={_ => flyToReserve(layer)} key={"layer" + id} className="listingText">{layer.properties.name} </div>
+        );
+      });
+    } else {
+      return (
+        <div></div>
+      )
+    }
+  }
+  return (
+    <div className='sidebar'>
+      <div className='heading'>
+        <h1>Naturschutzgebiete in Bayern</h1>
+      </div>
+      <div id='listings' className='listings'>
+        {getNatureReserveList()}
+      </div>
+    </div>
+  )
 }
 
-useEffect(() => {
-  getNatureLayer();
-}, []);
+function App() {
 
+  const [getLayer, setLayer] = useState(null);
+  const [viewport, setViewport] = useState(INITIAL_VIEW_STATE);
+ 
+  const goToMunich = useCallback(() => {
+    setViewport({
+      longitude: 11.5820,
+      latitude: 48.1351,
+      zoom: 14,
+      pitch: 0,
+      bearing: 0,
+      transitionDuration: 8000,
+      transitionInterpolator: new FlyToInterpolator()
+    })
+  }, []);
+
+  async function getNatureLayer() {
+    const response = await fetch(DATA_URL);
+    const json = await response.json();
+    setLayer(json);
+  }
+
+  useEffect(() => {
+    getNatureLayer();
+  }, []);
   const layers = [
     new GeoJsonLayer({
       id: 'nature-layer',
@@ -43,15 +96,20 @@ useEffect(() => {
     })
   ];
   return (
-    <div className="App">
-      <DeckGL
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
-        layers={layers}>
-        <InteractiveMap apitoken={apitoken} reuseMap preventStyleDiffing={true} mapStyle={MAP_STYLE}>
-        </InteractiveMap>
-      </DeckGL>
-    </div>
+    <React.Fragment>
+      <div className="map">
+        <DeckGL
+          initialViewState={viewport}
+          controller={true}
+          layers={layers}>
+          <StaticMap
+            apitoken={apitoken} reuseMap preventStyleDiffing={true} mapStyle={MAP_STYLE}>
+          </StaticMap>
+        </DeckGL>
+        <button className="floatButton" onClick={goToMunich}>München</button>
+      </div>
+      <LayerList layers={getLayer} viewport={setViewport}></LayerList>
+    </React.Fragment>
   );
 }
 
